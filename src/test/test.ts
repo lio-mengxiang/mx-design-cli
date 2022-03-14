1ac1306cc5e158c4fcad36636c07b69fd33f71b4
lio-mengxiang <1334196450@qq.com>
2022-03-14 14:30:02 +0800

    fix: 修改testconfig路径

--- src/test/test.ts
+++ src/test/test.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { ITestConfig } from "../interface";
 
 export default ({ updateSnapshot, coverage, watch }: Partial<ITestConfig>) => {
-  const configFile = path.join(__dirname, "../config/jestConfig/");
+  const configFile = path.join(__dirname, "../config/jestConfig/index.js");
   const args = [require.resolve("jest/bin/jest"), `--config=${configFile}`];
   updateSnapshot && args.push("-u");
   coverage && args.push("--coverage");
