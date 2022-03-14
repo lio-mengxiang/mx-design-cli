0328fca5d6973ba9c378a08d8e91fc5eb8287db8
lio-mengxiang <1334196450@qq.com>
2022-03-14 10:11:48 +0800

    fix: 修改test文件路径问题

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,9 +1,10 @@
 import execa from "execa";
+import path from "path";
 import { ITestConfig } from "../interface";
-import jestConfig from "../config/jestConfig";
 
 export default ({ updateSnapshot, coverage }: Partial<ITestConfig>) => {
-  const args = [require.resolve("jest/bin/jest"), `--config=${jestConfig}`];
+  const configFile = path.join(__dirname, `../config/jestConfig/index.ts`);
+  const args = [require.resolve("jest/bin/jest"), `--config=${configFile}`];
   updateSnapshot && args.push("-u");
   coverage && args.push("--coverage");
   execa("node", args, { stdio: "inherit" });
