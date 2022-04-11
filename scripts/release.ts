66a9d01b89f83c33630b1b988ef68b9bf6a8d499
lio-mengxiang <1334196450@qq.com>
2022-04-11 11:43:01 +0800

    feat: 打包增加ts-node

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,2 +1,12 @@
-import defaultRelaseProcess from "@mx-design/release";
-defaultRelaseProcess();
+import {
+  getNextVersion,
+  gitPush,
+  build,
+  publishNpm,
+  updateVersion,
+  compose,
+} from "@mx-design/release";
+
+const middle = [getNextVersion, updateVersion, gitPush, build, publishNpm];
+
+compose(middle);
