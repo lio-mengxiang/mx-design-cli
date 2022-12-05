afe54801347c86fcd30b6d94f6f9742f82eb5f60
lio-mengxiang <1334196450@qq.com>
2022-12-05 15:49:19 +0800

    fix: update vite import

--- src/config/vite.config.js
+++ src/config/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+const defineConfig = require('vite');
+const react = require('@vitejs/plugin-react');
 
 export default () =>
   defineConfig({
